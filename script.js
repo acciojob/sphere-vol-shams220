dc573ce4b96143dbdb29f03bacf9359a0338ff60
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-23 19:10:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ function volume_sphere() {
 	const r = document.querySelector('input[name="radius"]');
 	const v = document.querySelector('input[name="volume"]');
 
+	
+    const radius = Number(r.value);
+
+    if (isNaN(radius) || radius <= 0) {
+        v.value = "Invalid radius";
+        return;
+    }
+
+
 	const submitBtn = document.querySelector('#submit');
 
 	submitBtn.addEventListener('click',()=>{
 
-		v.value =  (Number(4 / 3) * Math.PI * Math.pow(r.value, 3));
+		v.value =  (4 / 3) * Math.PI * Math.pow(r.value, 3);
 	})
   
 } 
