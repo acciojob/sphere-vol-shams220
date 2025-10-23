4b12d8f1f0080c2ed7792f1d3cb3ef37bcfc0573
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-23 19:00:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	const r = document.querySelector('input[name="radius"]');
+	const v = document.querySelector('input[name="volume"]');
+
+	const submitBtn = document.querySelector('#submit');
+
+	submitBtn.addEventListener('click',()=>{
+
+		v.value =  (Number(4 / 3) * Math.PI * Math.pow(r, 3));
+	})
   
 } 
 
